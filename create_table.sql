2ed63c3d699c15e037629ff487ee7d6ca8a3d2aa
Flavio S.  de Lima <flavio.fatesg@yahoo.com.br>
2016-07-05 17:23:01 -0300

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -8,3 +8,7 @@ CREATE TABLE Employee(
        city          VARCHAR(10),
        description   VARCHAR(15)
      );
+     
+     
+insert into Employee(id,first_name, last_name, start_date, end_Date,   salary,  City,       Description)
+      values(7,'David',    'Larry',    '19901231', '19980212', 7897.78,'New York',  'Manager');
