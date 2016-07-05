0ed33709928bddf31e5c31bb0f0b0eff84225f4c
Flavio S.  de Lima <flavio.fatesg@yahoo.com.br>
2016-07-05 17:18:17 -0300

    Create create_procedure.sql

---
+++ create_procedure.sql
@@ -0,0 +1,13 @@
+delimiter $$
+CREATE PROCEDURE sp_add_location
+  (in_location     VARCHAR(30),
+        in_first_name   VARCHAR(30),
+          in_last_name    VARCHAR(30),
+         description     VARCHAR(10))
+   MODIFIES SQL DATA
+BEGIN
+   INSERT INTO employee
+           (city,first_name,last_name,description)
+         VALUES
+          (in_location,in_first_name,in_last_name,description);
+END$$
