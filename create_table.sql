99deff60aa9e99cb86a25161a74f22a483cc0cb4
Flavio S.  de Lima <flavio.fatesg@yahoo.com.br>
2016-07-05 17:21:16 -0300

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employee(
+       id            int,
+       first_name    VARCHAR(15),
+       last_name     VARCHAR(15),
+       start_date    DATE,
+       end_date      DATE,
+       salary        FLOAT(8,2),
+       city          VARCHAR(10),
+       description   VARCHAR(15)
+     );
