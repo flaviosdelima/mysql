3335e0604a9ad0695dee12f9f2bd5a89f7909e03
Flavio S.  de Lima <flavio.fatesg@yahoo.com.br>
2016-07-05 17:24:48 -0300

    Update create_procedure.sql

--- create_procedure.sql
+++ create_procedure.sql
@@ -1,16 +1,15 @@
-delimiter $$
-CREATE PROCEDURE sp_add_location
-  (in_location     VARCHAR(30),
-        in_first_name   VARCHAR(30),
-          in_last_name    VARCHAR(30),
-         description     VARCHAR(10))
-   MODIFIES SQL DATA
-BEGIN
-   INSERT INTO employee
-           (city,first_name,last_name,description)
+ delimiter $$
+  CREATE PROCEDURE sp_add_location
+             (in_location     VARCHAR(30),
+                in_first_name   VARCHAR(30),
+               in_last_name    VARCHAR(30),
+                description     VARCHAR(10))
+         MODIFIES SQL DATA
+     BEGIN
+       INSERT INTO employee
+          (city,first_name,last_name,description)
          VALUES
           (in_location,in_first_name,in_last_name,description);
-END$$
-
-insert into Employee(id,first_name, last_name, start_date, end_Date,   salary,  City,       Description)
-     values(7,'David',    'Larry',    '19901231', '19980212', 7897.78,'New York',  'Manager');
+    END$$
+ 
+delimiter ;
